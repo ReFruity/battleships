ac2e1d0563682e9271133bb380078c45f9edac8e
Антон Истомин <istomanton@gmail.com>
2015-04-07 21:56:02 +0500

    bugfixing + no bad shots occur

--- SampleAi/Program.cs
+++ SampleAi/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace SampleAi
 {
@@ -24,9 +25,10 @@ namespace SampleAi
 		{
 			var random = new Random();
 		    var aim = new Point(0, 0);
-            var boardSize = new Size(20, 20);
+		    var boardSize = new Size(0, 0);
 		    var nonTargetCells = new HashSet<Point>();
 		    var hotspots = new Stack<Point>();
+		    var woundedCells = new HashSet<Point>();
 
 			while (true)
 			{
@@ -47,40 +49,55 @@ namespace SampleAi
 			        case "Init": 
                         nonTargetCells.Clear();
                         hotspots.Clear();
-			            aim = NextCell(boardSize, nonTargetCells, hotspots, random);
-                        boardSize.Width = int.Parse(message[1]);
+                        woundedCells.Clear();
+			            boardSize.Width = int.Parse(message[1]);
 			            boardSize.Height = int.Parse(message[2]);
-                        break;
+			            break;
 
                     case "Miss":
                         nonTargetCells.Add(aim);
-                        aim = NextCell(boardSize, nonTargetCells, hotspots, random);
                         break;
 
                     case "Wound":
     			        GetOffsetCells(aim, boardSize, diagonals).ToList().ForEach(cell => nonTargetCells.Add(cell));
                         nonTargetCells.Add(aim);
                         hotspots.Push(aim);
-                        aim = NextCell(boardSize, nonTargetCells, hotspots, random);
+			            woundedCells.Add(aim);
 			            break;
 
                     case "Kill":
                         GetOffsetCells(aim, boardSize, neighbours).ToList().ForEach(cell => nonTargetCells.Add(cell));
                         nonTargetCells.Add(aim);
-                        hotspots.Push(aim);
-                        aim = NextCell(boardSize, nonTargetCells, hotspots, random);
+			            woundedCells.Add(aim);
+			            MarkDeadShipAdjacency(aim, boardSize, woundedCells, nonTargetCells);
 			            break;
 			    }
 
+                aim = NextCell(boardSize, nonTargetCells, hotspots, random);
 			    Console.WriteLine("{0} {1}", aim.X, aim.Y);
 			}
 		}
 
 	    private static bool WithinBoard(Point cell, Size size)
 	    {
-	        var withinVertically = 0 <= cell.X && cell.X < size.Height;
-	        var withinHorizontally = 0 <= cell.Y && cell.Y < size.Width;
+	        var withinVertically = 0 <= cell.X && cell.X < size.Width;
+	        var withinHorizontally = 0 <= cell.Y && cell.Y < size.Height;
 	        return withinVertically && withinHorizontally;
+	    }
+
+	    private static void MarkDeadShipAdjacency(Point aim, Size boardSize, 
+            ICollection<Point> woundedCells, ICollection<Point> nonTargetCells)
+	    {
+	        GetOffsetCells(aim, boardSize, adjacency).Where(woundedCells.Contains).ToList().ForEach(toMark =>
+	        {
+                var direction = new Size(toMark) - new Size(aim);
+	            while (woundedCells.Contains(toMark) && WithinBoard(toMark, boardSize))
+	            {
+	                toMark += direction;
+	            }
+                if (WithinBoard(toMark, boardSize))
+                    nonTargetCells.Add(toMark);
+	        });
 	    }
 
 	    private static Point NextCell(Size size, ICollection<Point> excluded, Stack<Point> hotspots, Random random)
@@ -94,7 +111,7 @@ namespace SampleAi
 	        }
 
 	        Point nextCell;
-	        do { nextCell = new Point(random.Next(size.Height), random.Next(size.Width)); } 
+	        do { nextCell = new Point(random.Next(size.Width), random.Next(size.Height)); } 
             while (excluded.Contains(nextCell));
 	        return nextCell;
 	    }
